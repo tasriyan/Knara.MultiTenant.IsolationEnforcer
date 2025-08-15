b14f2056ce3f8df6dbb4a1297f4d653eaceb0554
Tatyana Asriyan <tasriyan@hotmail.com>
2025-08-14 21:21:00 -0500

    Added cross-tenant manager and middleware.

---
+++ Multitenant.Enforcer/AspnetCore/Middleware/AddMiddleware.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Multitenant.Enforcer.AspnetCore;
+
+public static class AddMiddleware
+{
+	public static IApplicationBuilder UseMultiTenantIsolation(this IApplicationBuilder app)
+	{
+		return app.UseMiddleware<TenantContextMiddleware>();
+	}
+}
