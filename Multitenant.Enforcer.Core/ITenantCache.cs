67dd2eae832c9ccb9619d604c3963cd7bc3455bb
Tatyana Asriyan <tasriyan@hotmail.com>
2025-08-18 14:50:19 -0500

    Refactoring: added definitions for tenant cache and tenant store. Wil work on MutiTenantOptions later.

---
+++ Multitenant.Enforcer.Core/ITenantCache.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Multitenant.Enforcer.Core;
+
+public interface ITenantCache
+{
+	Task<T?> GetAsync<T>(string cacheKey, CancellationToken cancellationToken = default);
+	Task SetAsync<T>(string cacheKey, T data, TimeSpan? expiry, CancellationToken cancellationToken = default);
+	Task SetAsync<T>(string cacheKey, T data, MemoryCacheEntryOptions options, CancellationToken cancellationToken = default);
+	Task RemoveAsync(string cacheKey, CancellationToken cancellationToken = default);
+}
---
+++ Multitenant.Enforcer.Core/ITenantStore.cs
@@ -0,0 +1,17 @@
+﻿namespace Multitenant.Enforcer.Core;
+
+/// <summary>
+/// Provides methods for retrieving tenant-related data, including active tenant IDs and information.
+/// </summary>
+/// <remarks>This interface is designed to support multi-tenant applications by enabling the retrieval of
+/// tenant-specific data, such as active tenant IDs and detailed tenant information. Implementations of this interface
+/// should ensure thread safety and handle cancellation tokens appropriately to support asynchronous
+/// operations.</remarks>
+public interface ITenantStore
+{
+	Task<TenantInfo?> GetTenantInfoByDomainAsync(string domain, CancellationToken cancellationToken = default);
+
+	Task<TenantInfo?> GetTenantInfoAsync(Guid tenantId, CancellationToken cancellationToken);
+
+	Task<TenantInfo[]> GetAllActiveTenantsAsync(CancellationToken cancellationToken);
+}
