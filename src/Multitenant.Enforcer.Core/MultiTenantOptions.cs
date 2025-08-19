eda06cdf3be11719edd912ce92875fa965e0f011
Tatyana Asriyan <tasriyan@hotmail.com>
2025-08-18 21:40:56 -0500

    Moved core lib into src folder

---
+++ src/Multitenant.Enforcer.Core/MultiTenantOptions.cs
@@ -0,0 +1,8 @@
+namespace Multitenant.Enforcer.Core;
+
+public class MultiTenantOptions
+{
+	public bool CacheTenantResolution { get; set; } = true;
+	public int CacheExpirationMinutes { get; set; } = 5;
+	public static MultiTenantOptions DefaultOptions { get; } = new MultiTenantOptions();
+}
