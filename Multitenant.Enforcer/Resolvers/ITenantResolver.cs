a15494ad8749416c95045921af62ba13ca9b315a
Tatyana Asriyan <tasriyan@hotmail.com>
2025-08-18 21:39:19 -0500

    Deleted old enforcer lib.

--- Multitenant.Enforcer/Resolvers/ITenantResolver.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Http;
-using Multitenant.Enforcer.Core;
-
-namespace Multitenant.Enforcer.Resolvers;
-
-public interface ITenantResolver
-{
-	Task<TenantContext> ResolveTenantAsync(HttpContext context, CancellationToken cancellationToken);
-}
